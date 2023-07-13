49ecdb9f3019150bf00118b8244a4aabcf0adc19
yiu tsoi <abaddon@yius-MacBook-Air.local>
2023-07-13 22:49:07 +0800

    new start script

--- http.js
+++ http.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!\n')
 })
 
+app.get('/new', (req, res)=>{
+  res.send('you start a new page!');
+  console.log('getting new page');
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
