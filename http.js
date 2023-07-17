8965d1771f1849cfffff382736bff807cd1f84dd
yiu tsoi <abaddon@yius-MacBook-Air.local>
2023-07-17 13:58:14 +0800

    successfully applied logger for all pages

--- http.js
+++ http.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const newpage = require('./newpage.js')
 const root = require('./root.js')
-const morgan = require('morgan')
 const app = express()
 const router = express.Router()
 const port = 80
-const path = require('path')
-const fs = require('fs')
+
+var logger = require('./logger.js')
 
 // show current time
 // router.use((req, res, next)=> {
@@ -27,11 +26,7 @@ const fs = require('fs')
 // 	}
 // )
 
-var logStream = fs.createWriteStream(path.join(__dirname+'access.log'), {
-    flags: 'a'
-})
-
+app.use(logger)
 app.use('/new', newpage)
 app.use('/', root)
-app.use(morgan('dev', {stream: logStream}))
 app.listen(port)
--- root.js
+++ root.js
@@ -1,18 +1,8 @@
 const express = require('express')
-const morgan = require('morgan')
 const router = express()
 
 function print(text) {
     console.log(text)
-}
-
-const consoleLog = (req, res, next) => {
-    console.log('connected to the root page.!')
-    next()
-}
-
-const greetingPage = (req, res, next) => {
-    res.send('Root page')
 }
 
 const gettingPage = (req, res, next) => {
@@ -25,5 +15,4 @@ router.use('/', (req, res, next) => {
 })
 
 router.get('/', gettingPage)
-
 module.exports = router
